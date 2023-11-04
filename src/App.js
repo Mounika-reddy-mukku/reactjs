56377c8d583943f69368a774d044050a55042af0
Mounika-reddy-mukku <mounikareddtmukku@gmail.com>
2023-11-04 11:54:09 +0530

    git push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import CompA from "./CompA"
 
-function App(){
-    return <div>
-        <h2>App component</h2>
-        <CompA/>
-    </div>
+import React from 'react';
+import Message from './State Mg/Message';
+class App extends React.Component{
+    render(){
+        return <React.Fragment>
+            <h2>App Component</h2>
+            <Message/>  
+        </React.Fragment>
+    }
 }
 export default App
